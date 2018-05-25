225b2815e0b5bee5b935091f0cd609bf3f16e4d9
Emran Hosen <emranict@gmail.com>
2018-05-25 11:35:40 +0600

    php comments

---
+++ language_reference/basic_syntax/php_comments.php
@@ -0,0 +1,11 @@
+<?php
+    echo 'This is a test'; // This is a one-line c++ style comment
+    /* This is a multi line comment
+       yet another line of comment */
+    echo 'This is yet another test';
+    echo 'One Final Test'; # This is a one-line shell-style comment
+
+    // another comment is documentation comment
+
+  /*nesting of multiline comment is not possible in php*/
+?>
