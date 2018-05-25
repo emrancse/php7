5b73eecc2b3dc795b924b029862a035f53c4e6e6
Emran Hosen <emranict@gmail.com>
2018-05-25 11:06:51 +0600

    escaping from html

---
+++ language_reference/basic_syntax/escapingFromHtml.php
@@ -0,0 +1,9 @@
+<!-- <p>This is going to be ignored by PHP and displayed by the browser.</p>
+//<?php echo 'While this is going to be parsed.'; ?>
+<p>This will also be ignored by PHP and displayed by the browser.</p>  -->
+
+<?php $expression=false;if ($expression == true): ?>
+  This will show if the expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>
