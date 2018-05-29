30ad6ce6d8b401f35ba5546daab990adbcc820d1
Emran Hosen <emranict@gmail.com>
2018-05-29 06:08:29 +0600

    type hinting basic

--- database/pdo_database.php
+++ database/pdo_database.php
@@ -1,19 +1,16 @@
 <?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=php7_practice','root','');
+$sql = "SELECT * FROM users";
+$query = $pdo->query($sql);
 
-$query = $pdo->query('SELECT * FROM users');
-
-// $row = $query->fetch(PDO::FETCH_ASSOC);
 // $row = $query->fetch(PDO::FETCH_BOTH);
+// $row = $query->fetch(PDO::FETCH_ASSOC);
 // $row = $query->fetch(PDO::FETCH_OBJ);
 $row = $query->fetch(PDO::FETCH_NUM);
 
 echo "<pre>";
 print_r($row);
 echo "</pre>";
-
-
-
 
 ?>
---
+++ fucntion/typeHinting.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+  => There are two types of type hinting.
+     1. argument type hinting
+     3. return type hinting
+  => php supports 9 data types in type hinting.
+     1.Class/interface
+     2.self
+     3.array
+    4.callable
+    5.bool
+    6.float
+    7.int
+    8.string
+    9.iterable
+=>     
+
+*/
+
+
+
+
+
+?>
