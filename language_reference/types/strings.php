8b0ecafdd9ed053cbf964be8204157e7fa926ac3
Emran Hosen <emranict@gmail.com>
2018-05-30 23:43:46 +0600

    single quote strings in php

--- language_reference/types/strings.php
+++ language_reference/types/strings.php
@@ -6,9 +6,33 @@ Note: As of PHP 7.0.0, there are no particular restrictions regarding the length
  single quoted
  double quoted
  heredoc syntax
- nowdoc syntax (since PHP 5.3.0) 
+ nowdoc syntax (since PHP 5.3.0)
 
 */
+// single quoted
+
+echo 'this is a simple string'."<br/>";
+
+echo 'You can also have embedded newlines in
+strings this way as it is
+okay to do'."<br/>";
+
+// Outputs: Arnold once said: "I'll be back"
+echo 'Arnold once said: "I\'ll be back"'."<br/>";
+
+// Outputs: You deleted C:\*.*?
+echo 'You deleted C:\\*.*?'."<br/>";
+
+// Outputs: You deleted C:\*.*?
+echo 'You deleted C:\*.*?'."<br/>";
+
+// Outputs: This will not expand: \n a newline
+echo 'This will not expand: \n a newline'."<br/>";
+
+// Outputs: Variables do not $expand $either
+echo 'Variables do not $expand $either'."<br/>";
+
+
 
 
 
