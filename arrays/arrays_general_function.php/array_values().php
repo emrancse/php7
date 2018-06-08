b3a29547b3a6c26ef56e906033bdcbb62a49f631
Emran Hosen <emranict@gmail.com>
2018-06-08 23:39:02 +0600

    array_values() function

---
+++ arrays/arrays_general_function.php/array_values().php
@@ -0,0 +1,9 @@
+<?php
+$student= ["name"=>"Mamnun Bin Masud","age"=>4,"mobile"=>"01766882299"];
+
+echo "<pre>";
+echo "Print Only Array Values\n";
+print_r(array_values($student));
+echo "</pre>";
+
+?>
