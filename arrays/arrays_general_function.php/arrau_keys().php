f67dbfac361c620a8fa103c214a4b52b0d2a8589
Emran Hosen <emranict@gmail.com>
2018-06-08 23:35:59 +0600

    array_keys().php

---
+++ arrays/arrays_general_function.php/arrau_keys().php
@@ -0,0 +1,10 @@
+<?php
+$student= ["name"=>"Mamnun Bin Masud","age"=>4,"mobile"=>"01766882299"];
+
+echo "Print Only Array Keys"."<br/>";
+echo "<pre>";
+print_r(array_keys($student));
+echo "</pre>";
+
+
+?>
