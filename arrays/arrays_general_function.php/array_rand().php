a1fc8c0dac1b3c2eef6fd043b75fa6de52b1d1d9
Emran Hosen <emranict@gmail.com>
2018-06-09 22:18:25 +0600

    array_rand() function

---
+++ arrays/arrays_general_function.php/array_rand().php
@@ -0,0 +1,9 @@
+<?php
+$input = array("oranges", "apples", "pears","Mango","Banana");
+echo "Exmple of array_rand() Function<br/>";
+$randomly_pick = array_rand($input,2);
+
+echo $randomly_pick[0],"<br/>";
+echo $randomly_pick[1],"<br/>";
+
+?>
