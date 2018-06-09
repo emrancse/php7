dc7f4c1d47c433d5b55aef9af9861ccd46750960
Emran Hosen <emranict@gmail.com>
2018-06-09 22:07:28 +0600

    array_flip() upadate and array_reverse() function

--- arrays/arrays_general_function.php/array_flip().php
+++ arrays/arrays_general_function.php/array_flip().php
@@ -2,7 +2,8 @@
 $input = array("oranges", "apples", "pears");
 echo "Exmple of array_flip() Function\n";
 $flipped = array_flip($input);
-
+echo "<pre>";
 print_r($flipped);
+echo "</pre>";
 
 ?>
---
+++ arrays/arrays_general_function.php/array_reverse().php
@@ -0,0 +1,10 @@
+<?php
+$input = array("oranges", "apples", "pears");
+
+echo "Exmple of array_reverse() Function"."<br/>";
+
+$reversed = array_reverse($input);
+echo "<pre>";
+print_r($reversed);
+echo "</pre>";
+?>
