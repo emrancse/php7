a4e6937064de28387c8299869aeea8b19fdd13da
Emran Hosen <emranict@gmail.com>
2018-06-09 22:21:37 +0600

    shuffle() function

---
+++ arrays/arrays_general_function.php/shuffle().php
@@ -0,0 +1,9 @@
+<?php
+$input = array("oranges", "apples", "pears","Mango","Banana");
+
+echo "Exmple of shuffle() Function<br/>";
+
+shuffle($input);
+echo "<pre>";
+print_r($input);
+echo "</pre>";
